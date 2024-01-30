19c91809facdd781bd47737ec412a8076d9ae778
ayyappansuruttaiyan <ayyappan.suruttaiyan@gmail.com>
2024-01-30 09:44:35 +0530

    loader file added

---
+++ src/Loader.jsx
@@ -0,0 +1,10 @@
+function Loader() {
+  return (
+    <div className="loader-container">
+      {/* Add your loader component or use a library like react-loader-spinner */}
+      Loading...
+    </div>
+  );
+}
+
+export default Loader;
